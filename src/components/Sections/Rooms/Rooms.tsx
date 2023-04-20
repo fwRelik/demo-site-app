8bcd7b88554f5108cd6b1e5ab52a97ca0934099e
fwRelik <relik.fw@gmail.com>
2023-04-20 12:29:23 +0300

    Rooms sections complete

--- src/components/Sections/Rooms/Rooms.tsx
+++ src/components/Sections/Rooms/Rooms.tsx
@@ -1,27 +1,93 @@
-import Image from 'next/image';
 import styles from './Rooms.module.scss';
 import { RoomsProps } from './Rooms.props';
 
 import { SectionTitles } from '@/components/SectionTitles/SectionTitles';
+import { Button } from '@/components/Button/Button';
 
 export const Rooms = ({ ...props }: RoomsProps): JSX.Element => {
 	return (
-		<section className={styles.find_apartment}>
+		<section className={styles.rooms}>
 			<div className='container'>
 				<div className={styles.wrapper}>
 					<div>
-						<SectionTitles level={'h5'} pre_title>
+						<SectionTitles className={styles.pre_title} level={'h5'} pre_title>
 							The Luxury Hotel
 						</SectionTitles>
-						<SectionTitles level={'h3'} section_title>
+						<SectionTitles className={styles.section_title} level={'h3'} section_title>
 							Rooms & Suites
 						</SectionTitles>
 					</div>
 					<div className={styles.room_cards}>
 						<div className={styles.card}>
 							<div className={styles.card_inner}>
-								<div className={styles.card_front}>Front</div>
-								<div className={styles.card_back}>Back</div>
+								<div className={styles.card_front}>
+									<div className={styles.price}>From $90/night</div>
+									<div className={styles.text_wrapper}>
+										<div className={styles.title}>Deluxe Double Rooms</div>
+										<div className={styles.description}>38m2/2 beds/1 bathroom</div>
+									</div>
+								</div>
+								<div className={styles.card_back}>
+									<div className={styles.text_wrapper}>
+										<div className={styles.title}>Deluxe Double Rooms</div>
+										<div className={styles.description}>
+											Architecture viverra tristique ustoni an missten vitae diam neque nivamus
+											the aestan the atene artines arinianu ateli ine finibus viverra nec lacus.
+										</div>
+										<Button href='#' className={styles.button}>
+											Book Now
+										</Button>
+									</div>
+								</div>
+								<img className={styles.overlay_image} src='/images/card1.jpg' alt='' />
+							</div>
+						</div>
+						<div className={styles.card}>
+							<div className={styles.card_inner}>
+								<div className={styles.card_front}>
+									<div className={styles.price}>From $90/night</div>
+									<div className={styles.text_wrapper}>
+										<div className={styles.title}>Deluxe Double Rooms</div>
+										<div className={styles.description}>38m2/2 beds/1 bathroom</div>
+									</div>
+								</div>
+								<div className={styles.card_back}>
+									<div className={styles.text_wrapper}>
+										<div className={styles.title}>Deluxe Double Rooms</div>
+										<div className={styles.description}>
+											Architecture viverra tristique ustoni an missten vitae diam neque nivamus
+											the aestan the atene artines arinianu ateli ine finibus viverra nec lacus.
+										</div>
+										<Button href='#' className={styles.button}>
+											Book Now
+										</Button>
+									</div>
+								</div>
+								<img className={styles.overlay_image} src='/images/card2.jpg' alt='' />
+							</div>
+						</div>
+						<div className={styles.card}>
+							<div className={styles.card_inner}>
+								<div className={styles.card_front}>
+									<div className={styles.price}>From $90/night</div>
+									<div className={styles.text_wrapper}>
+										<div className={styles.title}>Deluxe Double Rooms</div>
+										<div className={styles.description}>38m2/2 beds/1 bathroom</div>
+									</div>
+								</div>
+								<div className={styles.card_back}>
+									<div className={styles.text_wrapper}>
+										<div className={styles.title}>Deluxe Double Rooms</div>
+										<div className={styles.description}>
+											Architecture viverra tristique ustoni an missten vitae diam neque nivamus
+											the aestan the atene artines arinianu ateli ine finibus viverra nec lacus.
+										</div>
+										<Button href='#' className={styles.button}>
+											Book Now
+										</Button>
+									</div>
+								</div>
+								<img className={styles.overlay_image} src='/images/card3.jpg' alt='' />
 							</div>
 						</div>
 					</div>
