464c578c050ad216f88ddb9d20404410b092bd28
fwRelik <relik.fw@gmail.com>
2023-04-23 16:37:37 +0300

    Room page initial value fix

--- src/pages/admin/pages/index.tsx
+++ src/pages/admin/pages/index.tsx
@@ -20,8 +20,59 @@ export function Pages() {
 		roomName: 'Room Name',
 		roomType: 'type_1',
 		roomDescription: 'Room Description',
-		roomImagePath: '/public/images/room1.jpg',
-		languagePack: {},
+		roomImagePath: '/images/room1.jpg',
+		languagePack: {
+			resources: {
+				en: {
+					translation: {
+						header_home: 'Home',
+						header_about: 'About',
+						header_rooms_suites: 'Rooms & Suites',
+						header_facilities: 'Facilities',
+						header_offers: 'Offers',
+						header_contact: 'Contact',
+						header_button_text: 'Get a Quote',
+						welcome_pre_title: 'This is Luxury Hotel',
+						welcome_title: 'Superior Hotel in World',
+						welcome_description:
+							'A wonderful serenity has taken possession of my entrie soul, like these sweet mornings of spring which I enjoy with my whole heart.',
+						welcome_button_text: 'Rooms & Suites',
+					},
+				},
+				ru: {
+					translation: {
+						header_home: 'Дом',
+						header_about: 'О',
+						header_rooms_suites: 'Номера и люксы',
+						header_facilities: 'Удобства',
+						header_offers: 'Предложения',
+						header_contact: 'Контакт',
+						header_button_text: 'Получить предложение',
+						welcome_pre_title: 'Это роскошный отель',
+						welcome_title: 'Улучшенный отель в мире',
+						welcome_description:
+							'Чудесная безмятежность овладела моей преддверной душой, как эти сладостные весенние утра, которыми я наслаждаюсь всем сердцем.',
+						welcome_button_text: 'Номера и люксы',
+					},
+				},
+				tr: {
+					translation: {
+						header_home: 'Ev',
+						header_about: 'Hakkında',
+						header_rooms_suites: 'Odalar ve Süitler',
+						header_facilities: 'Tesisler',
+						header_offers: 'Teklifler',
+						header_contact: 'Temas etmek',
+						header_button_text: 'Teklif Alın',
+						welcome_pre_title: 'Burası Lüks Otel',
+						welcome_title: 'Dünyada Üstün Otel',
+						welcome_description:
+							'Tüm kalbimle tadını çıkardığım bu tatlı bahar sabahları gibi harika bir dinginlik tüm ruhumu ele geçirdi.',
+						welcome_button_text: 'Odalar ve Süitler',
+					},
+				},
+			},
+		},
 	};
 
 	const createPage = () => {
